60f35d8cd03cdf47cc387eeb80e1adea3dbc61eb
Yed Star <chstokrp@gmail.com>
2022-04-26 02:46:02 -0700

    gql login

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 import gql from "graphql-tag";
 
-export const loginUser = gql`
+export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
