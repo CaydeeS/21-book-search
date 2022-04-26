473362e306536de8dc9688c2940f2fff060045cc
Yed Star <chstokrp@gmail.com>
2022-04-26 02:33:50 -0700

    update clarify

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,11 +7,11 @@ const resolvers = {
     me: async (parent, args, context) => {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id }).select(
-          "-__v -password"
+          " "
         );
         return userData;
       }
-      throw new AuthenticationError("Not logged in");
+      
     },
   },
   Mutation: {
